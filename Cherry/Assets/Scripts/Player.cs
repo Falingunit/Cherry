06a5a8a989cdff0b618bbfb6308edb42f4dd1bfe
Falingunit <sbaniruddh1@gmail.com>
2022-03-02 20:59:11 +0530

    Added basic player movement

--- Cherry/Assets/Scripts/Player.cs
+++ Cherry/Assets/Scripts/Player.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField] private float speed = 7, jumpVel = 5;
+
+    private Rigidbody2D rigidbody;
+    private BoxCollider2D boxCollider;
+    private SpriteRenderer spriteRenderer;
+
+    private float dirX;
+    private bool isLanded = false;
+
+
+    private void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+
+        PlayerMovement();
+        AnimationController();
+
+        if (Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0f, Vector2.down, .1f, LayerMask.GetMask("Ground")))
+        {
+            isLanded = true;
+        }
+        else 
+        { 
+            isLanded = false;
+        }
+
     }
+
+    private void AnimationController()
+    {
+        if (dirX > .1f)
+        {
+            spriteRenderer.flipX = false;
+        }else if (dirX < -.1f)
+        {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private void PlayerMovement()
+    {
+        //Horizontal Movement
+        dirX = Input.GetAxisRaw("Horizontal");
+
+        rigidbody.velocity = new Vector2(dirX * speed, rigidbody.velocity.y); ;
+
+        //Jumping
+        if (Input.GetButtonDown("Jump") && isLanded)
+        {
+            rigidbody.velocity = new Vector2(rigidbody.velocity.x, jumpVel);
+        }
+
+    }
+
 }
